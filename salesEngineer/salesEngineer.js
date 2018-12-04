f1d986171fb909631474d047d3c929a76ca8b16a
Kevin ODea <kodea@hubspot.com>
2018-12-04 06:29:51 -0500

    updates

--- salesEngineer/salesEngineer.js
+++ salesEngineer/salesEngineer.js
@@ -3,42 +3,52 @@
 //parse the hubspotutk value after the = sign
 //assign the value to a variable called hutk
 
-var hutk;
+var utk;
 var hsContact;
+var hsContext = new Object();
+
 
 $(document).ready( function getCookie () {
     var cookieArray = document.cookie.split(';');
     for (i = 0; i < cookieArray.length; i++) {
         var cookieArray2 = cookieArray[i].split("=");
         if (cookieArray2[0] == ("hubspotutk")) {
-            hutk = cookieArray2[1];
+            utk = cookieArray2[1];
             break;
         }
-    }});
+        else if (cookieArray2[0] == (" " + "hubspotutk")) {
+          utk = cookieArray2[1];
+          break;
+        };
+    hsContext.hutk = utk;
+    hsContext.pageUrl = window.location.href;
+    hsContext.pageName = document.title;
+    console.log(hsContext);
+  }});
 
 // on form submit convert data to required format for HubSpot
 // send data through HubSpot Form
 
-$("#salesEngineer").submit(function(event) {
-    event.preventDefault();
-    meetingRequest = ($("#salesEngineer").serializeArray());
-    hsContact = '{ "fields": ' + JSON.stringify(meetingRequest) + ', ' + '"context": {' + '"hutk": ' + '"' + hutk + '",' + '},' + ' }';
-      $.ajax({
-        type: "POST",
-        url: "https://api.hsforms.com/submissions/v3/integration/submit/416460/90d3ce76-0563-4f6a-91f7-e2a0415ad9e9",
-        contentType: "application/json",
-        data: hsContact,
-      success: function(){
-          var card = document.createElement("P");
-          var confirmation = `<div>
-            <h1>Your Request Has Been Submitted!</h1>
-            </div>`;
-          var submission = document.createTextNode(confirmation);
-          card.appendChild(confirmation);
-          document.getElementById("salesEngineer").appendChild(card);
-    $("#salesEngineer").empty();
-    $("#salesEngineer").append(confirmation);
-      },
-    });
-  });
+// $("#salesEngineer").submit(function(event) {
+//     event.preventDefault();
+//     meetingRequest = ($("#salesEngineer").serializeArray());
+//     hsContact = '{ "fields": ' + JSON.stringify(meetingRequest) + ', ' + '"context": {' + '"hutk": ' + '"' + utk + '",' + '},' + ' }';
+//       $.ajax({
+//         type: "POST",
+//         url: "https://api.hsforms.com/submissions/v3/integration/submit/416460/90d3ce76-0563-4f6a-91f7-e2a0415ad9e9",
+//         contentType: "application/json",
+//         data: hsContact,
+//       success: function(){
+//           var card = document.createElement("P");
+//           var confirmation = `<div>
+//             <h1>Your Request Has Been Submitted!</h1>
+//             </div>`;
+//           var submission = document.createTextNode(confirmation);
+//           card.appendChild(confirmation);
+//           document.getElementById("salesEngineer").appendChild(card);
+//     $("#salesEngineer").empty();
+//     $("#salesEngineer").append(confirmation);
+//       },
+//     });
+//   });
 
