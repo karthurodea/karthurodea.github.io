37bb3e73b51e386c293fdf75a746bf9599d2fae6
Kevin ODea <kodea@hubspot.com>
2018-12-05 13:23:38 -0500

    updates

--- salesEngineer/salesEngineer.js
+++ salesEngineer/salesEngineer.js
@@ -1,9 +1,25 @@
-//initialize references carousel
+// Enable Carousel Indicators
 
-// $('.carousel').carousel({
-//   interval: 2000
-// });
+$(".item1").click(function(){
+  $("#myCarousel").carousel(0);
+});
 
+$(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+$(".item3").click(function(){
+  $("#myCarousel").carousel(2);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+
+$(".right").click(function(){
+    $("#myCarousel").carousel("next");
+});
 
 //separate the cookie string into an array
 //loop through the array to find the hubspotutk
