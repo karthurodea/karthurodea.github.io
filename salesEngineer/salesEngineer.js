d626407d8a4596a7212fe6fc4c7743dc34fe943b
Kevin ODea <kodea@hubspot.com>
2018-12-08 09:46:06 -0500

    js testing

--- salesEngineer/salesEngineer.js
+++ salesEngineer/salesEngineer.js
@@ -9,7 +9,32 @@ var hsContact;
 var hsContext = new Object();
 
 
-$(document).ready( function getCookie () {
+// $(document).ready( function getCookie () {
+//     var cookieArray = document.cookie.split(';');
+//     for (i = 0; i < cookieArray.length; i++) {
+//         var cookieArray2 = cookieArray[i].split("=");
+//         if (cookieArray2[0] == ("hubspotutk")) {
+//             utk = cookieArray2[1];
+//             hsContext.hutk = utk;
+//             hsContext.pageUri = window.location.href;
+//             hsContext.pageName = document.title;
+//           break;
+//         }
+//         else if (cookieArray2[0] == (" " + "hubspotutk")) {
+//             utk = cookieArray2[1];
+//             hsContext.hutk = utk;
+//             hsContext.pageUri = window.location.href;
+//             hsContext.pageName = document.title;
+//           break;
+//         };
+//   }});
+
+// on form submit convert data to required format for HubSpot
+// send data through HubSpot Form
+
+$("#salesEngineer").submit(function(event) {
+    event.preventDefault();
+    function getCookie () {
     var cookieArray = document.cookie.split(';');
     for (i = 0; i < cookieArray.length; i++) {
         var cookieArray2 = cookieArray[i].split("=");
@@ -27,13 +52,7 @@ $(document).ready( function getCookie () {
             hsContext.pageName = document.title;
           break;
         };
-  }});
-
-// on form submit convert data to required format for HubSpot
-// send data through HubSpot Form
-
-$("#salesEngineer").submit(function(event) {
-    event.preventDefault();
+  }};
     meetingRequest = ($("#salesEngineer").serializeArray());
     hsContact = '{ "fields": ' + JSON.stringify(meetingRequest) + ', ' + '"context": ' + JSON.stringify(hsContext) + ',' + '"skipValidation": false' + ' }';
       $.ajax({
